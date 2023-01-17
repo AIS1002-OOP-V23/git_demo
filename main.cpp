1e3b54fe7f53fe3f2312fe4edf2ec2f93b6f05ae
Lars Ivar Hatledal <laht@ntnu.no>
2023-01-17 14:29:08 +0100

    Delegated to a free function and renamed universe (#3)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+void printHello() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, Universe!" << std::endl;
+    printHello();
     return 0;
 }
